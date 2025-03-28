1e20939edb4782e5acf6b32dd0616f00667a364c
Meera Bhola <meerabhola3@gmail.com>
2025-03-28 00:27:37 -0400

    created flatlist

--- components/Home.js
+++ components/Home.js
@@ -1,9 +1,33 @@
-import { Text, View, StyleSheet } from 'react-native';
+import React from 'react';
+import { Text, View, StyleSheet, FlatList } from 'react-native';
+
+const exercises= [
+    {
+        id: '1',
+        title: 'Push-ups'   
+    },
+    {
+        id: '2',
+        title: 'Running'
+    },
+];
+
+const Item = ({title}) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
 
 export default function Home() {
+
     return (
         <View style={styles.container}>
           <Text style={styles.header}>Exercise Hero</Text>
+          <FlatList
+            data={exercises}
+            renderItem={({item}) => <Item title={item.title} />}
+            keyExtractor={item => item.id}
+          />
         </View>
       );
 }
@@ -13,9 +37,24 @@ const styles = StyleSheet.create({
       flex: 1,
       backgroundColor: '#BC9BEC',
       alignItems: 'center',
+      justifyContent: 'center',
     },
     header: {
+        marginTop: 10,
         color: '#fff',
-        fontSize: 40,
-    }
+        fontSize: 50,
+        fontFamily: 'Helvetica',
+    },
+    item: {
+        backgroundColor: '#000',
+        padding: 15,
+        marginVertical: 20,
+        marginHorizontal: 16,
+        borderRadius: 10
+      },
+    title: {
+        fontSize: 30,
+        color: '#fff',
+        textAlign: 'center'
+    },
   });
